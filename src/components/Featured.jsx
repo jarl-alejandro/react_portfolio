d979411dfa72a30d4dbe3f07cd517bdecd3006a5
Rodrick Bloomfield <rodrickbloomfield@yahoo.com>
2018-12-10 19:59:44 -0500

    feat: Add transfer featured section to homepage

---
+++ src/components/Featured.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Page from '../templates/Page'
+import projects from '../data/projects'
+
+const SlideShowItem = ({ name, description, for: _for, image }) => {
+
+  return (
+    <li>
+      <a>
+        <div className='uk-position-cover uk-position-medium' data-uk-slideshow-parallax='scale: 0.2, 1, 0.2;'>
+          <img className='uk-position-center uk-padding-large' src={image} alt={name} data-uk-cover />
+        </div>
+        <div className='uk-overlay uk-overlay-primary uk-position-bottom uk-text-justify uk-transition-slide-bottom'>
+          <h3 className='uk-margin-remove'>{name}</h3>
+          <p className='uk-margin-remove'>{description}</p>
+          <p className='uk-text-meta uk-text-right'>{_for}</p>
+        </div>
+      </a>
+    </li>
+  );
+}
+
+
+
+export default () => {
+
+  const featuredProjects = projects.filter(project => project.featured);
+
+  return (
+    <Page>
+      <h2 className='uk-heading-line'>Featured Projects</h2>
+
+      <div data-uk-slideshow=' autoplay: true; autoplay-interval: 5000; pause-on-hover'>
+        <div className='uk-position-relative uk-padding'>
+
+          {/* Slide show list */}
+          <ul className='uk-slideshow-items'>
+            {featuredProjects.map(project => (<SlideShowItem key={project.name} {...project} />))}
+          </ul>
+
+          {/* Slideshow Nav */}
+          <a className='uk-position-center-left uk-position-small uk-hidden-hover' href="#" data-uk-icon='icon: chevron-left; ratio: 3' data-uk-slideshow-item='previous'></a>
+          <a className='uk-position-center-right uk-position-small uk-hidden-hover' href="#" data-uk-icon='icon: chevron-right; ratio: 3' data-uk-slideshow-item='next'></a>
+
+          <ul className='uk-slideshow-nav uk-dotnav uk-flex-center uk-margin'></ul>
+        </div>
+      </div>
+
+    </Page>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import Page from '../templates/Page';
 
 import Elevator from '../components/ElevatorPitch';
 import pitch from '../data/elevator'
+import Featured from '../components/Featured';
 
 export default () => (
   <Page home>
     <Elevator pitch={pitch} />
+    <Featured />
   </Page>
 )
