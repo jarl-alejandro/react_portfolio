a70cfa5e33c368c68dc20202fe0d8cbf1eb3f31a
Rodrick Bloomfield <rodrickbloomfield@yahoo.com>
2018-12-12 10:55:59 -0500

    fix: Line up of title between pages

--- src/templates/Page.jsx
+++ src/templates/Page.jsx
@@ -5,13 +5,13 @@ const Section = styled.section`
   padding: ${({home}) => home ? '3em 0' : '3em 1em' };
   
   @media screen and (min-width: 960px) {
-    padding: ${({home}) => home ? '0' : '2' }em;
+    padding: ${({home}) => home ? '2em 0' : '2em' };
   }
 `;
 
 export default ({ children, title, scroll, home }) => (
   <Section scroll={scroll} home={home}>
-      {title && <h1 className='uk-heading-primary'>{title}</h1>}
+      {title && <h1 className='uk-heading-line uk-text-center'><span>{title}</span></h1>}
       {children}
   </Section>
 )
