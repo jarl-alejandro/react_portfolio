4df09404809af335489b0b8ec9dbdbf33f7484ff
Rodrick Bloomfield <rodrickbloomfield@yahoo.com>
2018-12-14 20:32:41 -0500

    feat: Add site link and lightbox view(Portfolio)

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,11 +3,11 @@ import Badges from './Badges';
 
 export default ({ name, image, for: _for, link, description, repo, languages, frameworks, utilities }) => (
   <div className='uk-card uk-card-large uk-card-default'>
-    
+
     {/* Card Header */}
     <div className='uk-card-header'>
       <div className="uk-grid-small uk-flex-middle" data-uk-grid>
-        
+
         {/* Thumbnail */}
         <div className='uk-width-auto'>
           <img className='uk-border-circle' width='40' height='40' src={image} alt={name} />
@@ -15,7 +15,7 @@ export default ({ name, image, for: _for, link, description, repo, languages, fr
 
         {/* Summary info */}
         <div className='uk-width-expand uk-flex uk-flex-between uk-flex-middle uk-flex-wrap'>
-          
+
           {/* Title/meta */}
           <div>
             <h3 className='uk-card-title uk-margin-remove-bottom'>{name}</h3>
@@ -24,15 +24,23 @@ export default ({ name, image, for: _for, link, description, repo, languages, fr
 
           {/* Project links */}
           <div className='uk-flex-inline'>
-            
-            {/* Site link */}
+
+            {/* In page view */}
             {link && (
-              <div data-uk-lightbox>
-                <a className='uk-margin-small-right uk-flex uk-flex-column uk-link-text'  href={link} data-caption={description} data-type='iframe' data-alt={name} >
+              <>
+                <div data-uk-lightbox>
+                  <a className='uk-margin-small-right uk-flex uk-flex-column uk-link-text' href={link} data-caption={description} data-type='iframe' data-alt={name} >
+                    <span className='uk-icon-button' data-uk-icon='play'></span>
+                    <span className='uk-text-center uk-text-meta'>View</span>
+                  </a>
+                </div>
+
+                {/* Site Link */}
+                <a className='uk-margin-small-right uk-flex uk-flex-column uk-link-text' href={link} data-caption={description} data-type='iframe' data-alt={name} >
                   <span className='uk-icon-button' data-uk-icon='world'></span>
                   <span className='uk-text-center uk-text-meta'>Site</span>
                 </a>
-              </div>
+              </>
             )}
 
             {/* Github repo link */}
@@ -47,7 +55,7 @@ export default ({ name, image, for: _for, link, description, repo, languages, fr
 
     {/* Card body */}
     <div className='uk-card-body'>
-      
+
       {/* Project Image(s) */}
       <div className='uk-card-media-bottom'>
         <img src={image} alt={name} />
@@ -56,11 +64,11 @@ export default ({ name, image, for: _for, link, description, repo, languages, fr
 
     {/* Card Footer */}
     <div className='uk-card-footer uk-text-center'>
-      
+
       {/* Drawer: Project details */}
       <ul data-uk-accordion>
         <li>
-          
+
           {/* Drawer toggle */}
           <div className='uk-accordion-title'>
             <button className='uk-button uk-button-text'>Details</button>
@@ -68,7 +76,7 @@ export default ({ name, image, for: _for, link, description, repo, languages, fr
 
           {/* Project details */}
           <div className="uk-accordion-content">
-            
+
             {/* Description data */}
             <p>{description}</p>
 
